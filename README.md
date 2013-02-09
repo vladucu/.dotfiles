20baf892e36a4a99d4dbf1e6c814f76a8e289316
Vlad Vanca <vlad@sweettoothhq.com>
2013-02-09 21:21:42 +0200

    Adding sync script, install dependencies script & .zshrc dotfile

--- README.md
+++ README.md
@@ -1,4 +1,6 @@
 .dotfiles
 =========
 
-Personal .dotfiles git project, used on Ubuntu 12.10
+Personal dotfiles git project, used on Ubuntu 12.10.
+
+Most of it copied from Paul Irish's [dotfiles project](https://github.com/mathiasbynens/dotfiles/) which is great, so make sure you check that first.
---
+++ install-deps.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+# Install zsh
+# sudo apt-get update --quiet
+sudo apt-get install --quiet --assume-yes zsh
+
+# Set zsh as your default shell:
+chsh -s `which zsh`
+
+# https://github.com/jamiew/git-friendly
+# the `push` command which copies the github compare URL to my clipboard is heaven
+sudo bash < <( curl https://raw.github.com/jamiew/git-friendly/master/install.sh)
+
+# https://rvm.io
+# rvm for the rubiess
+# curl -L https://get.rvm.io | bash -s stable --ruby
+
+# https://github.com/rupa/z
+# z, oh how i love you
+mkdir -p $PWD/z
+curl https://raw.github.com/rupa/z/master/z.sh > $PWD/z/z.sh
+chmod +x $PWD/z/z.sh
+# also consider moving over your current .z file if possible. it's painful to rebuild :)
